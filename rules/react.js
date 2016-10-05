bb49ad2d91461a2555cfc04ec3d9e6b1e8df8c90
James G. Kim <jgkim@jayg.org>
2016-10-05 16:27:43 +0900

    Turn off `react/no-unused-prop-types`

--- rules/react.js
+++ rules/react.js
@@ -7,5 +7,8 @@ module.exports = {
     // Forbid certain propTypes (any, array, object)
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
     'react/forbid-prop-types': 'off',
+    // Prevent unused propType definitions
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
+    'react/no-unused-prop-types': 'off',
   },
 };
